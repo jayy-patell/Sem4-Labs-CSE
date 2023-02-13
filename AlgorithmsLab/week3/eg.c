b4678f86c0d4463da187b175677e4104a5304d07
Jay <95573258+jayy-patell@users.noreply.github.com>
2023-02-13 16:43:00 +0530

    Add files via upload

---
+++ AlgorithmsLab/week3/eg.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void SelectionSort(int *a, int n){
+    int i,j,min_ind,temp;
+    int opcount=0;
+    for(i=0;i<n-1;i++){
+        min_ind = i;
+        for(j=i+1;j<n;j++){
+            ++opcount;
+            if(a[j]<a[min_ind]){
+                min_ind=j;
+            }
+        }
+        temp = a[i];
+        a[i]=a[min_ind];
+        a[min_ind] = temp;
+    }
+    printf("\nOpcount is: %d", opcount);
+}
+
+int main(){
+    int arr[5];
+    printf("Enter 5 elements in array: ");
+    for(int i=0;i<5;i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("Before sorting:");
+    for(int i=0;i<5;i++){
+        printf("%d", arr[i]);
+    }
+    SelectionSort(arr,5);
+    printf("After selection:");
+    for(int i=0;i<5;i++){
+        printf("%d", arr[i]);
+    }
+    return 0;
+}
---
+++ AlgorithmsLab/week3/q1.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void Bubbly(int* a, int n){
+    int i,j,opcount=0,temp;
+    for(i=0;i<(n-1);i++){
+        for(j=0;j<(n-i-1);j++){
+            opcount++;
+            if(a[j]>a[j+1]){
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
+    printf("\nOpcount is: %d", opcount);
+}
+
+int main(){
+    int n;
+    printf("Enter dimensions: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements in array: ");
+    for(int i=0;i<n;i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("Before sorting:");
+    for(int i=0;i<n;i++){
+        printf("%d", arr[i]);
+    }
+    Bubbly(arr,n);
+    printf("After sorting:");
+    for(int i=0;i<n;i++){
+        printf("%d", arr[i]);
+    }
+    return 0;
+}
