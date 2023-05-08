26e3dfc130ecb87d394bb4a6843929e3e4632820
Jay <95573258+jayy-patell@users.noreply.github.com>
2023-05-08 23:31:00 +0530

    Edited DAA lab

--- AlgorithmsLab/week7/q2.c
+++ AlgorithmsLab/week7/q2.c
@@ -1,153 +1,363 @@
-#include<stdio.h>
-#include<stdlib.h>
+// AVL tree implementation in C
 
-struct node
-{
-	struct node *left,*right;
-	int data;
-	int balance;
+#include <stdio.h>
+#include <stdlib.h>
+
+// Create Node
+struct Node {
+  int key;
+  struct Node *left;
+  struct Node *right;
+  int height;
 };
 
-typedef struct node Node;
+int max(int a, int b);
 
-int max(int a,int b)
-{
-	return a>b? a: b;
+// Calculate height
+int height(struct Node *N) {
+  if (N == NULL)
+    return 0;
+  return N->height;
 }
 
-int height(Node *root)
-{
-	if(root==NULL)
-		return 0;
-	return max(height(root->left),height(root->right))+1;
+int max(int a, int b) {
+  return (a > b) ? a : b;
 }
 
-void balance_factor(Node *root,Node *st[],int *top)
-{
-	if(root==NULL)
-		return;
-	if(root->left==NULL && root->right==NULL)
-	{
+// Create a node
+struct Node *newNode(int key) {
+  struct Node *node = (struct Node *)
+    malloc(sizeof(struct Node));
+  node->key = key;
+  node->left = NULL;
+  node->right = NULL;
+  node->height = 1;
+  return (node);
+}
+
+// Right rotate
+struct Node *rightRotate(struct Node *y) {
+  struct Node *x = y->left;
+  struct Node *T2 = x->right;
+
+  x->right = y;
+  y->left = T2;
+
+  y->height = max(height(y->left), height(y->right)) + 1;
+  x->height = max(height(x->left), height(x->right)) + 1;
+
+  return x;
+}
+
+// Left rotate
+struct Node *leftRotate(struct Node *x) {
+  struct Node *y = x->right;
+  struct Node *T2 = y->left;
+
+  y->left = x;
+  x->right = T2;
+
+  x->height = max(height(x->left), height(x->right)) + 1;
+  y->height = max(height(y->left), height(y->right)) + 1;
+
+  return y;
+}
+
+// Get the balance factor
+int getBalance(struct Node *N) {
+  if (N == NULL)
+    return 0;
+  return height(N->left) - height(N->right);
+}
+
+// Insert node
+struct Node *insertNode(struct Node *node, int key) {
+  // Find the correct position to insertNode the node and insertNode it
+  if (node == NULL)
+    return (newNode(key));
+
+  if (key < node->key)
+    node->left = insertNode(node->left, key);
+  else if (key > node->key)
+    node->right = insertNode(node->right, key);
+  else
+    return node;   //reaches its correct place
+
+  // Update the balance factor of each node and
+  // Balance the tree
+  node->height = 1 + max(height(node->left),
+               height(node->right));
+
+  int balance = getBalance(node);
+  if (balance > 1 && key < node->left->key)
+    return rightRotate(node);
+
+  if (balance < -1 && key > node->right->key)
+    return leftRotate(node);
+
+  if (balance > 1 && key > node->left->key) {
+    node->left = leftRotate(node->left);
+    return rightRotate(node);
+  }
+
+  if (balance < -1 && key < node->right->key) {
+    node->right = rightRotate(node->right);
+    return leftRotate(node);
+  }
+
+  return node;
+}
+
+struct Node *minValueNode(struct Node *node) {
+  struct Node *current = node;
+
+  while (current->left != NULL)
+    current = current->left;
+
+  return current;
+}
+
+// Delete a nodes
+struct Node *deleteNode(struct Node *root, int key) {
+  // Find the node and delete it
+  if (root == NULL)
+    return root;
+
+  if (key < root->key)
+    root->left = deleteNode(root->left, key);
+
+  else if (key > root->key)
+    root->right = deleteNode(root->right, key);
+
+  else {
+    if ((root->left == NULL) || (root->right == NULL)) {
+      struct Node *temp = root->left ? root->left : root->right;
+
+      if (temp == NULL) {
+        temp = root;
+        root = NULL;
+      } else
+        *root = *temp;
+      free(temp);
+    } else {
+      struct Node *temp = minValueNode(root->right);
+
+      root->key = temp->key;
+
+      root->right = deleteNode(root->right, temp->key);
+    }
+  }
+
+  if (root == NULL)
+    return root;
+
+  // Update the balance factor of each node and
+  // balance the tree
+  root->height = 1 + max(height(root->left),
+               height(root->right));
+
+  int balance = getBalance(root);
+  if (balance > 1 && getBalance(root->left) >= 0)
+    return rightRotate(root);
+
+  if (balance > 1 && getBalance(root->left) < 0) {
+    root->left = leftRotate(root->left);
+    return rightRotate(root);
+  }
+
+  if (balance < -1 && getBalance(root->right) <= 0)
+    return leftRotate(root);
+
+  if (balance < -1 && getBalance(root->right) > 0) {
+    root->right = rightRotate(root->right);
+    return leftRotate(root);
+  }
+
+  return root;
+}
+
+// Print the tree
+void printPreOrder(struct Node *root) {
+  if (root != NULL) {
+    printf("%d ", root->key);
+    printPreOrder(root->left);
+    printPreOrder(root->right);
+  }
+}
+
+int main() {
+  struct Node *root = NULL;
+
+  root = insertNode(root, 2);
+  root = insertNode(root, 1);
+  root = insertNode(root, 7);
+  root = insertNode(root, 4);
+  root = insertNode(root, 5);
+  root = insertNode(root, 3);
+  root = insertNode(root, 8);
+
+  printPreOrder(root);
+
+  root = deleteNode(root, 3);
+
+  printf("\nAfter deletion: ");
+  printPreOrder(root);
+
+  return 0;
+}
+
+
+
+// #include<stdio.h>
+// #include<stdlib.h>
+
+// struct node
+// {
+// 	struct node *left,*right;
+// 	int data;
+// 	int balance;
+// };
+
+// typedef struct node Node;
+
+// int max(int a,int b)
+// {
+// 	return a>b? a: b;
+// }
+
+// int height(Node *root)
+// {
+// 	if(root==NULL)
+// 		return 0;
+// 	return max(height(root->left),height(root->right))+1;
+// }
+
+// void balance_factor(Node *root,Node *st[],int *top)
+// {
+// 	if(root==NULL)
+// 		return;
+// 	if(root->left==NULL && root->right==NULL)
+// 	{
 		
-		return;
-	}
-	balance_factor(root->left,st,top);
-	int bf = height(root->left)-height(root->right);
-	root->balance=bf;
-	if(bf>1 || bf<-1)
-		st[++(*top)]=root;
-	balance_factor(root->right,st,top);
-}
+// 		return;
+// 	}
+// 	balance_factor(root->left,st,top);
+// 	int bf = height(root->left)-height(root->right);
+// 	root->balance=bf;
+// 	if(bf>1 || bf<-1)
+// 		st[++(*top)]=root;
+// 	balance_factor(root->right,st,top);
+// }
 
-Node *leftrotate(Node *root)
-{
-	Node *z=root;
-	Node *y=root->right;
-	z->right=y->left;
-	y->left=z;
-	return y;
-}
+// Node *leftrotate(Node *root)
+// {
+// 	Node *z=root;
+// 	Node *y=root->right;
+// 	z->right=y->left;
+// 	y->left=z;
+// 	return y;
+// }
 
-Node *rightrotate(Node *root)
-{
-	Node *z=root;
-	Node *y=root->left;
-	z->left=y->right;
-	y->right=z;
-	return y;
-}
+// Node *rightrotate(Node *root)
+// {
+// 	Node *z=root;
+// 	Node *y=root->left;
+// 	z->left=y->right;
+// 	y->right=z;
+// 	return y;
+// }
 
-Node *avl(Node *root,int key)
-{
-	Node *st[10];
-	int top=-1;
-	balance_factor(root,st,&top);
-	if(top==-1)
-		return root;
-	//search the node
-	//printf("hi\n");
-	Node *snode;
-	if(top==1)
-		snode=st[top-1];
-	else
-		snode=st[top];
-	Node *nd=root;
-	//printf("hi\n");
-	//left case
-	//printf("%d ",snode->data);
-	if(snode->balance>1 && key<nd->left->data)
-		root=rightrotate(snode);
-	//right case
-	else if(snode->balance<-1 && key>nd->right->data)
-		root=leftrotate(snode);
-	//left right case
-	else if(snode->balance>1 && key>nd->left->data)
-	{
-		snode->left =leftrotate(snode->left);
-		root=rightrotate(snode);
-	}
-	//right left case
-	else if(snode->balance<1 && key<nd->right->data)
-	{
-		snode->right =rightrotate(snode->right);
-		root=leftrotate(snode);
-	}
-	return root;
-}
+// Node *avl(Node *root,int key)
+// {
+// 	Node *st[10];
+// 	int top=-1;
+// 	balance_factor(root,st,&top);
+// 	if(top==-1)
+// 		return root;
+// 	//search the node
+// 	//printf("hi\n");
+// 	Node *snode;
+// 	if(top==1)
+// 		snode=st[top-1];
+// 	else
+// 		snode=st[top];
+// 	Node *nd=root;
+// 	//printf("hi\n");
+// 	//left case
+// 	//printf("%d ",snode->data);
+// 	if(snode->balance>1 && key<nd->left->data)
+// 		root=rightrotate(snode);
+// 	//right case
+// 	else if(snode->balance<-1 && key>nd->right->data)
+// 		root=leftrotate(snode);
+// 	//left right case
+// 	else if(snode->balance>1 && key>nd->left->data)
+// 	{
+// 		snode->left =leftrotate(snode->left);
+// 		root=rightrotate(snode);
+// 	}
+// 	//right left case
+// 	else if(snode->balance<1 && key<nd->right->data)
+// 	{
+// 		snode->right =rightrotate(snode->right);
+// 		root=leftrotate(snode);
+// 	}
+// 	return root;
+// }
 
-Node *newNode(int data,Node *root)
-{
-	Node *node=(Node*)malloc(sizeof(Node));
-	node->left=NULL;
-	node->right=NULL;
-	node->data=data;
-	node->balance=0;
-	if(root==NULL)
-	{
-		return node;
-	}
+// Node *newNode(int data,Node *root)
+// {
+// 	Node *node=(Node*)malloc(sizeof(Node));
+// 	node->left=NULL;
+// 	node->right=NULL;
+// 	node->data=data;
+// 	node->balance=0;
+// 	if(root==NULL)
+// 	{
+// 		return node;
+// 	}
 
-	Node *nd=root;
-	Node *parent;
-	while(nd)
-	{
-		parent=nd;
-		if(nd->data<data)
-			nd=nd->right;
-		else
-			nd=nd->left;
-	}
-	if(parent->data<data)
-		parent->right=node;
-	else
-		parent->left=node;
-	root=avl(root,data);
-	return root;
-}
+// 	Node *nd=root;
+// 	Node *parent;
+// 	while(nd)
+// 	{
+// 		parent=nd;
+// 		if(nd->data<data)
+// 			nd=nd->right;
+// 		else
+// 			nd=nd->left;
+// 	}
+// 	if(parent->data<data)
+// 		parent->right=node;
+// 	else
+// 		parent->left=node;
+// 	root=avl(root,data);
+// 	return root;
+// }
 
-void print(Node *root)
-{
-	if(root==NULL)
-		return;
-	printf("%d ",root->data);
-	print(root->left);
-	print(root->right);
-}
+// void print(Node *root)
+// {
+// 	if(root==NULL)
+// 		return;
+// 	printf("%d ",root->data);
+// 	print(root->left);
+// 	print(root->right);
+// }
 
-int main()
-{
-	int n;
-	Node *root=NULL;
-	printf("Enter the Number of Nodes of The AVL Tree : \n");
-	scanf("%d",&n);
-	int x;
-	for(int i=0;i<n;i++)
-	{
-		printf("Enter the %d Node in the AVL Tree \n",(i+1));
-		scanf("%d",&x);
-		root=newNode(x,root);
-	}
-	printf("The AVL Tree Inserted has the Preorder Traversal given by : \n");
-	print(root);
-	printf("\n");
-}
+// int main()
+// {
+// 	int n;
+// 	Node *root=NULL;
+// 	printf("Enter the Number of Nodes of The AVL Tree : \n");
+// 	scanf("%d",&n);
+// 	int x;
+// 	for(int i=0;i<n;i++)
+// 	{
+// 		printf("Enter the %d Node in the AVL Tree \n",(i+1));
+// 		scanf("%d",&x);
+// 		root=newNode(x,root);
+// 	}
+// 	printf("The AVL Tree Inserted has the Preorder Traversal given by : \n");
+// 	print(root);
+// 	printf("\n");
+// }
--- AlgorithmsLab/week8/q2.c
+++ AlgorithmsLab/week8/q2.c
@@ -1,68 +1,139 @@
+// Heap Sort in C
+
 #include <stdio.h>
-#include <stdlib.h>
 
-int op = 0;
-
-void heapify(int h[], int l, int n)
+void swap(int* a, int* b)
 {
-    int i, k, v, heapify, j;
-    for(i = (n/2); i>=l; i--)
-    {
-        k = i; v = h[k]; heapify = 0;
-        while(heapify == 0 && 2*k <= n)
-        {
-            j = 2*k;
-            op++;
-            if(j<n)
-                if(h[j]<h[j+1])
-                    j = j+1;
-            if(v>=h[j])
-                heapify = 1;
-            else
-            {
-                h[k] = h[j];
-                k = j;
-            }
-        }
-        h[k] = v;
-    }
-    return;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
-void HeapSort(int arr[], int n)
+// To heapify a subtree rooted with node i
+// which is an index in arr[].
+// n is size of heap
+void heapify(int arr[], int N, int i)
 {
-    int k = 0;
-    for(int i = 1; i<=n; i++)
-    {
-        heapify(arr, 1, n - k);
-        int temp = arr[1];
-        arr[1] = arr[n-k];
-        arr[n-k] = temp;
-        k++;
-    }
+	// Initialize largest as root
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+
+	if (left < N && arr[left] > arr[largest])
+		largest = left;
+
+	if (right < N && arr[right] > arr[largest])
+		largest = right;
+
+	if (largest != i) {
+		swap(&arr[i], &arr[largest]);
+
+		// Recursively heapify the affected sub-tree
+		heapify(arr, N, largest);
+	}
+}
+
+void heapSort(int arr[], int N)
+{
+	// Build max heap
+	for (int i = N / 2 - 1; i >= 0; i--)
+		heapify(arr, N, i);
+
+	// Heap sort
+	for (int i = N - 1; i >= 0; i--) {
+		swap(&arr[0], &arr[i]);
+
+		// Heapify root element to get highest element at root again
+		heapify(arr, i, 0);
+	}
+}
+
+void printArray(int arr[], int N)
+{
+	for (int i = 0; i < N; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
 }
 
 int main()
 {
-    int arr[20], n;
-   
-    printf("Enter the Number of Elements : \n");
-    scanf("%d", &n);
-    
-    printf("Enter the Elements : \n");
-    
-    for(int i = 1; i<=n; i++)
-        scanf("%d", &arr[i]);
-    
-    HeapSort(arr, n);
-    
-    printf("The Sorted List is : \n");
-    
-    for(int i = 1; i<=n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-    
-    printf("Count = %d\n", op);
+	int arr[] = { 12, 11, 13, 5, 6, 7 };
+	int N = sizeof(arr) / sizeof(arr[0]);
 
-    return 0;
+	heapSort(arr, N);
+	printf("Sorted array is\n");
+	printArray(arr, N);
 }
+
+
+
+
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int op = 0;
+
+// void heapify(int h[], int l, int n)
+// {
+//     int i, k, v, heapify, j;
+//     for(i = (n/2); i>=l; i--)
+//     {
+//         k = i; v = h[k]; heapify = 0;
+//         while(heapify == 0 && 2*k <= n)
+//         {
+//             j = 2*k;
+//             op++;
+//             if(j<n)
+//                 if(h[j]<h[j+1])
+//                     j = j+1;
+//             if(v>=h[j])
+//                 heapify = 1;
+//             else
+//             {
+//                 h[k] = h[j];
+//                 k = j;
+//             }
+//         }
+//         h[k] = v;
+//     }
+//     return;
+// }
+
+// void HeapSort(int arr[], int n)
+// {
+//     int k = 0;
+//     for(int i = 1; i<=n; i++)
+//     {
+//         heapify(arr, 1, n - k);
+//         int temp = arr[1];
+//         arr[1] = arr[n-k];
+//         arr[n-k] = temp;
+//         k++;
+//     }
+// }
+
+// int main()
+// {
+//     int arr[20], n;
+   
+//     printf("Enter the Number of Elements : \n");
+//     scanf("%d", &n);
+    
+//     printf("Enter the Elements : \n");
+    
+//     for(int i = 1; i<=n; i++)
+//         scanf("%d", &arr[i]);
+    
+//     HeapSort(arr, n);
+    
+//     printf("The Sorted List is : \n");
+    
+//     for(int i = 1; i<=n; i++)
+//         printf("%d ", arr[i]);
+//     printf("\n");
+    
+//     printf("Count = %d\n", op);
+
+//     return 0;
+// }
